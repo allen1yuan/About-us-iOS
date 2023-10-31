77675d08dd1539e1a769eb4d85bc0480ed6873f7
zijun yuan <zy2554@nyu.edu>
2023-10-30 20:32:44 -0500

    Initial Commit

---
+++ About us/About_usApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_usApp.swift
+//  About us
+//
+//  Created by zijun yuan on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct About_usApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
